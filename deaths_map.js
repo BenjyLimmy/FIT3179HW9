7bf5bcad0c883ee811fdfb58b4a82c01cc8c0faa
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-09-19 16:15:06 +0800

    upload map

---
+++ deaths_map.js
@@ -0,0 +1,4 @@
+var spec = "deaths_map.json";
+vegaEmbed('#deaths_map', spec).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
